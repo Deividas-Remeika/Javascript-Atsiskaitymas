49fd15f0f9fab1c2bd64b6a3ad62c28025fbb9dc
Deividas Remeika <97938850+Deividas-Remeika@users.noreply.github.com>
2022-03-22 09:33:29 +0200

    Įkeliu 3 užduoties atsakymą

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,25 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+document.querySelector("#btn").addEventListener("click", (e) => {
+  /*const zinute = document.getElementById("message");
+  if (zinute !== null) {
+    zinute.remove();
+  }*/
+  const output = document.querySelector("#output");
+  output.innerHTML = null;
+  fetch(ENDPOINT)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        output.innerHTML += `
+        <div>
+        <h3>Login: ${element.login}</h3>
+        <h3>Avatar URL: <a href="${element.avatar_url}">Click me</a></h3>
+        </div>
+        `;
+      });
+    });
+});
