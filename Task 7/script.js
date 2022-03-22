96ccb04c91fea2e836f59d6985af3f2381c2bffb
Deividas Remeika <97938850+Deividas-Remeika@users.noreply.github.com>
2022-03-22 10:50:26 +0200

    Įkeliu 7 užduoties atsakymą

--- Task 7/script.js
+++ Task 7/script.js
@@ -5,8 +5,15 @@ Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  marke: 'audi',
-  model: 'A6',
+  marke: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white'
+  color: "white",
 };
+
+function showObjectKeys(objektas) {
+  let keys = Object.keys(objektas);
+  console.log(keys);
+}
+
+showObjectKeys(audi);
