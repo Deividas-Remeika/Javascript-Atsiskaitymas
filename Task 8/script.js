33eadb1007d89283efc6297ed38d70523c71ee85
Deividas Remeika <97938850+Deividas-Remeika@users.noreply.github.com>
2022-03-22 11:10:27 +0200

    Įkeliu 8 užduoties atsakymą

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,32 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  sum = (pirmasSkaicius, antrasSkaicius) => {
+    let answer1 = pirmasSkaicius + antrasSkaicius;
+    return answer1;
+  };
+  subtraction = (pirmasSkaicius, antrasSkaicius) => {
+    let answer2 = pirmasSkaicius - antrasSkaicius;
+    return answer2;
+  };
+  multiplication = (pirmasSkaicius, antrasSkaicius) => {
+    let answer3 = pirmasSkaicius * antrasSkaicius;
+    return answer3;
+  };
+  division = (pirmasSkaicius, antrasSkaicius) => {
+    let answer4 = pirmasSkaicius / antrasSkaicius;
+    return answer4;
+  };
+}
+
+let testCalculator = new Calculator();
+console.log("Sudėties veiksmo atsakymas: " + testCalculator.sum(100, 150));
+console.log(
+  "Atimties veiksmo atsakymas: " + testCalculator.subtraction(250, 150)
+);
+console.log(
+  "Daugybos veiksmo atsakymas: " + testCalculator.multiplication(150, 2)
+);
+console.log("Dalybos veiksmo atsakymas: " + testCalculator.division(300, 3));
