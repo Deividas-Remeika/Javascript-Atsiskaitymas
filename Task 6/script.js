454e06ad894f37fc7795a8defd88e7fc7d5acc6c
Deividas Remeika <97938850+Deividas-Remeika@users.noreply.github.com>
2022-03-22 10:37:09 +0200

    Įkeliu 6 užduoties atsakymą

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,35 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+function getUserAverageAge(users) {
+  var allAges = [];
+  users.forEach((element) => {
+    allAges.push(element.age);
+  });
+  const average = allAges.reduce((a, b) => a + b) / allAges.length;
+  console.log("Vidutinis amžius: " + average);
+  console.log("Vidutinis amžius(suapvalintas): " + Math.floor(average));
+}
+
+getUserAverageAge(users);
+
+function getUsersNames(users) {
+  var userNames = [];
+  users.forEach((user) => {
+    userNames.push(user.name);
+  });
+  console.log("Visi vartotojų vardai: " + userNames);
+}
+
+getUsersNames(users);
