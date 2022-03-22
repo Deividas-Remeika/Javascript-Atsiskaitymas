37a413b4e1ab291e5932da23c37731f8ced550ab
Deividas Remeika <97938850+Deividas-Remeika@users.noreply.github.com>
2022-03-22 08:33:46 +0200

    Įkeliu 1 užduoties atsakymą

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,20 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.querySelector("#forma").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let svorisKg = e.target.elements.search.value;
+  let lb = svorisKg * 2.2046;
+  let g = svorisKg / 0.001;
+  let oz = svorisKg * 35.274;
+  let output = document.querySelector("#output");
+  output.innerHTML = `
+  <h1>Results</h1>
+  <div>Svoris svarais(lb) = ${lb}</div>
+  <div>Svoris gramais(g) = ${g}</div>
+  <div>Svoris uncijomis(oz) = ${oz}</div>
+  `;
+  output.style.textAlign = "center";
+  output.style.fontSize = "1.3em";
+});
